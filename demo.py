a4b8a854492d971fb47939f3b64a5cafd45d2837
SamuelCarmona83 <candiedpopcorn10@gmail.com>
2018-09-23 12:54:59 -0400

    working

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+import numpy as np
 from numpy import exp, array, random, dot
 
 
@@ -22,12 +23,13 @@ class NeuralNetwork():
     # This is the gradient of the Sigmoid curve.
     # It indicates how confident we are about the existing weight.
     def __sigmoid_derivative(self, x):
-        return x * (1 - x)
+        sigmoid_x = self.__sigmoid(dot(x, self.synaptic_weights))
+        return sigmoid_x * (1 - sigmoid_x)
 
     # We train the neural network through a process of trial and error.
     # Adjusting the synaptic weights each time.
     def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
-        for iteration in xrange(number_of_training_iterations):
+        for iteration in range(number_of_training_iterations):
             # Pass the training set through our neural network (a single neuron).
             output = self.think(training_set_inputs)
 
@@ -38,7 +40,7 @@ class NeuralNetwork():
             # Multiply the error by the input and again by the gradient of the Sigmoid curve.
             # This means less confident weights are adjusted more.
             # This means inputs, which are zero, do not cause changes to the weights.
-            adjustment = dot(training_set_inputs.T, error * self.__sigmoid_derivative(output))
+            adjustment = dot(training_set_inputs.T, error * self.__sigmoid_derivative(training_set_inputs))
 
             # Adjust the weights.
             self.synaptic_weights += adjustment
@@ -54,21 +56,21 @@ if __name__ == "__main__":
     #Intialise a single neuron neural network.
     neural_network = NeuralNetwork()
 
-    print "Random starting synaptic weights: "
-    print neural_network.synaptic_weights
+    print("Random starting synaptic weights:")
+    print(neural_network.synaptic_weights)
 
     # The training set. We have 4 examples, each consisting of 3 input values
     # and 1 output value.
-    training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
-    training_set_outputs = array([[0, 1, 1, 0]]).T
+    training_set_inputs = array([[-9, 8, -6], [-6, 5, 10], [-4, 0, 8], [0, 4, 7]])
+    training_set_outputs = array([[10, -10, 5, -5]]).T
 
     # Train the neural network using a training set.
     # Do it 10,000 times and make small adjustments each time.
     neural_network.train(training_set_inputs, training_set_outputs, 10000)
 
-    print "New synaptic weights after training: "
-    print neural_network.synaptic_weights
+    print("New synaptic weights after training:")
+    print(neural_network.synaptic_weights)
 
     # Test the neural network with a new situation.
-    print "Considering new situation [1, 0, 0] -> ?: "
-    print neural_network.think(array([1, 0, 0]))
+    print("Considering new situation [1, 0, 0] -> ?:")
+    print(neural_network.think(array([100,0,-0])))
